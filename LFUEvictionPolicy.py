092f2cd5e8bbe66d25a05b7561bc95f1f1d6f641
chir263 <31371125+chir263@users.noreply.github.com>
2024-07-15 19:21:33 +0530

    optimized LFU

--- LFUEvictionPolicy.py
+++ LFUEvictionPolicy.py
@@ -1,34 +1,41 @@
 from cache import EvictionPolicyBase
-from collections import OrderedDict, defaultdict
+from collections import defaultdict, OrderedDict
 from typing import Any
 import logging
 
 class LFUEvictionPolicy(EvictionPolicyBase):
     def __init__(self):
         self.frequency = defaultdict(int)
-        self.access_order = OrderedDict()
+        self.keys_by_frequency = defaultdict(OrderedDict)
+        self.min_frequency = 0
 
     def key_accessed(self, key: Any) -> None:
-        if key in self.access_order:
-            self.access_order.move_to_end(key)
+        
+        if key in self.frequency:
+            freq = self.frequency[key]
+            del self.keys_by_frequency[freq][key]
+            if not self.keys_by_frequency[freq]:
+                del self.keys_by_frequency[freq]
+                if self.min_frequency == freq:
+                    self.min_frequency += 1
+            self.frequency[key] += 1
+            freq = self.frequency[key]
         else:
-            self.access_order[key] = None
-        self.frequency[key] += 1
+            self.frequency[key] = 1
+            freq = 1
+            self.min_frequency = 1
+
+        self.keys_by_frequency[freq][key] = None
 
     def evict(self) -> Any:
-        min_freq = float('inf')
-        lfu_key = None
-
-        for key, freq in self.frequency.items():
-            if freq < min_freq:
-                min_freq = freq
-                lfu_key = key
-
-        if lfu_key is None:
+        if not self.keys_by_frequency:
             logging.error("LFU eviction failed: no key found")
             return None
 
-        del self.frequency[lfu_key]
-        del self.access_order[lfu_key]
+        lfu_key, _ = self.keys_by_frequency[self.min_frequency].popitem(last=False)
+        if not self.keys_by_frequency[self.min_frequency]:
+            del self.keys_by_frequency[self.min_frequency]
+            self.min_frequency += 1
 
+        del self.frequency[lfu_key]
         return lfu_key
